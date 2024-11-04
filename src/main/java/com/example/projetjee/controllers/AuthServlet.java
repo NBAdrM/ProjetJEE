7bb9c949bead908df695378a0b97922a578e68ce
Sao <julienhautecourt@gmail.com>
2024-11-04 17:30:56 +0100

    Basic configuration/mapping for Servlet

--- src/main/java/com/example/projetjee/controllers/AuthServlet.java
+++ src/main/java/com/example/projetjee/controllers/AuthServlet.java
@@ -3,6 +3,5 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-public class StudentServlet {
+public class AuthServlet {
 }
