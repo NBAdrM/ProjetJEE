56e410949bd8287cb1d5938991031762e3568786
Sao <julienhautecourt@gmail.com>
2024-11-17 19:43:25 +0100

    feat : add logger to servlet

--- src/main/java/com/example/projetjee/controllers/StudentServlet.java
+++ src/main/java/com/example/projetjee/controllers/StudentServlet.java
@@ -1,16 +1,12 @@
 package com.example.projetjee.controllers;
-import com.example.projetjee.utils.DbConnnect;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.logging.Logger;
 
 import com.example.projetjee.models.Student;
 import com.example.projetjee.utils.UserGenerator;
@@ -18,19 +14,24 @@ import com.example.projetjee.utils.DbConnnect;
 
 public class StudentServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private static final Logger logger = Logger.getLogger(StudentServlet.class.getName());
     /*
      * This method will be called when an admin will submit the form
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.info("Start student servlet");
         String lastName = request.getParameter("lastName");
         String firstName = request.getParameter("firstName");
         String email = request.getParameter("email");
         String address = request.getParameter("address");
         String report = request.getParameter("report");
         int id =-1; //Initialize the id to -1 to avoid null pointer exception, it will be updated after the insertion in the database
+        logger.info("request give \nlastName: " + lastName + "\nfirstName: " + firstName + "\nemail: " + email + "\naddress: " + address + "\nreport: " + report);
+
 
         //Check if one field is empty
         if (lastName == null || lastName.isEmpty() || firstName == null || firstName.isEmpty() || email == null || email.isEmpty() || address == null || address.isEmpty() ||  report == null || report.isEmpty()) {
+            logger.warning("Invalid parameters");
             request.setAttribute("error", "All fields are required");
             request.getRequestDispatcher("/studentForm.jsp").forward(request, response);
             return;
@@ -38,19 +39,27 @@ public class StudentServlet extends HttpServlet {
 
         try {
             //Generate Username and password and insert into the database
+            logger.info("Generating username and password");
             String username = UserGenerator.generateUsername(firstName, lastName);
             String password = UserGenerator.generatePassword();
 
-            //Student student = new Student(id, lastName, firstName, email, address, username, password, report);
 
-            //DbConnnect.addStudent(student.getfirstName,student.getlastName,student.getEmail,student.getAddress,student.getUsername,student.getPassword, student.getReport);
+            logger.info("Generating student instance");
+            Student student = new Student(id, lastName, firstName, email, address, username, password, true, report);
+            logger.info(student.toString());
+
+            //TODO : Mettre à jour addStudent pour inclure le report
+            logger.info("Adding student to database");
+            //DbConnnect.addStudent(student.getFirstName(),student.getLastName(),student.getEmail(),student.getAddress(),student.getUsername(),student.getPassword(), student.getReport());
+            logger.info("successfully added student to the database");
 
             //Give the student object to the jsp page
-            //request.setAttribute("student", student);
+            request.setAttribute("student", student);
             request.setAttribute("success", "Student created successfully");
             request.getRequestDispatcher("/home.jsp").forward(request, response);
 
         } catch (SQLException | ClassNotFoundException e) {
+            logger.warning("SQL Error: " + e.getMessage());
             throw new RuntimeException(e);
         }
     }
--- src/main/java/com/example/projetjee/controllers/TeacherServlet.java
+++ src/main/java/com/example/projetjee/controllers/TeacherServlet.java
@@ -7,29 +7,31 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.logging.Logger;
+
 
 import com.example.projetjee.models.Teacher;
 import com.example.projetjee.utils.DbConnnect;
 
 public class TeacherServlet extends HttpServlet {
-
     private static final long serialVersionUID = 1L;
+    private static final Logger logger = Logger.getLogger(TeacherServlet.class.getName());
     /*
      * This method will be called when an admin will submit the form
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.info("Start TeacherServlet");
         String lastName = request.getParameter("lastName");
         String firstName = request.getParameter("firstName");
         String email = request.getParameter("email");
         String address = request.getParameter("address");
         int id =-1; //Initialize the id to -1 to avoid null pointer exception, it will be updated after the insertion in the database
+        logger.info("request give \nlastName: " + lastName + "\nfirstName: " + firstName + "\nemail: " + email + "\naddress: " + address);
 
         //Check if one field is empty
         if (lastName == null || lastName.isEmpty() || firstName == null || firstName.isEmpty() || email == null || email.isEmpty() || address == null || address.isEmpty()) {
+            logger.warning("Invalid parameters");
             request.setAttribute("error", "All fields are required");
             request.getRequestDispatcher("/teacherForm.jsp").forward(request, response);
             return;
@@ -37,19 +39,24 @@ public class TeacherServlet extends HttpServlet {
 
         try {
             //Generate Username and password and insert into the database
+            logger.info("Generating username and password");
             String username = UserGenerator.generateUsername(firstName, lastName);
             String password = UserGenerator.generatePassword();
 
-            //Teacher teacher = new Teacher(id, lastName, firstName, email, address, username, password, report);
+            logger.info("Generating teacher instance");
+            Teacher teacher = new Teacher(id, lastName, firstName, email, address, username, password, true);
+            logger.info(teacher.toString());
 
-            //DbConnnect.addTeacher(teacher.getfirstName,teacher.getlastName,teacher.getEmail,teacher.getAddress,teacher.getUsername,teacher.getPassword,teacher.getReport);
+            logger.info("Adding student to database");
+            DbConnnect.addTeacher(teacher.getFirstName(),teacher.getLastName(),teacher.getEmail(),teacher.getAddress(),teacher.getUsername(),teacher.getPassword());
+            logger.info("successfully added student to the database");
 
             //Give the teacher object to the jsp page
-            //request.setAttribute("teacher", teacher);
+            request.setAttribute("teacher", teacher);
             request.setAttribute("success", "Teacher created successfully");
             request.getRequestDispatcher("/home").forward(request, response);
         } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
+            logger.severe("SQL Error: " + e.getMessage());
             throw new RuntimeException(e);
         }
     }
@@ -58,6 +65,7 @@ public class TeacherServlet extends HttpServlet {
         String id = request.getParameter("id");
 
         if (id == null || id.isEmpty()) {
+            logger.warning("Invalid parameters");
             request.setAttribute("error", "ID is required");
             request.getRequestDispatcher("/teacherForm.jsp").forward(request, response);
             return;
