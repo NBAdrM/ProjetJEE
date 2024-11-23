17be65b311ba2be99b2a899ab3ecdbca6192ae03
Sao <julienhautecourt@gmail.com>
2024-11-24 00:36:07 +0100

    fix : stmt.executeQuery for update, feat : add getStudentsByName

--- src/main/java/com/example/projetjee/utils/DbConnnect.java
+++ src/main/java/com/example/projetjee/utils/DbConnnect.java
@@ -244,7 +244,7 @@ public class DbConnnect {
 
         stmt.setBoolean(1, false);
         stmt.setInt(2,id);
-        stmt.executeQuery();
+        stmt.executeUpdate();
         stmt.close();
         conn.close();
     }
@@ -499,5 +499,37 @@ public class DbConnnect {
         conn.close();
 
         return generatedId;
+    }
+
+    public static List<Student> getStudentsByName(String lastName,String firstName) throws SQLException, ClassNotFoundException {
+        Connection conn = initializeDatabase();
+
+        String query = "SELECT * FROM Student AS s INNER JOIN Person AS p ON s.id=p.id WHERE p.last_name LIKE ? OR p.first_name LIKE ?";
+
+        PreparedStatement stmt = conn.prepareStatement(query);
+
+        stmt.setString(1, lastName);
+        stmt.setString(2, firstName);
+
+        ResultSet rs = stmt.executeQuery();
+        List<Student> students = new ArrayList<>();
+
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String firstname = rs.getString("first_name");
+            String lastname = rs.getString("last_name");
+            String email = rs.getString("email");
+            String address = rs.getString("address");
+            String username = rs.getString("username");
+            String password = rs.getString("password");
+            Boolean active = rs.getBoolean("active");
+            String report = rs.getString("report");
+
+            students.add(new Student(id, firstname,lastname, email,address,null,null,active,report));
+        }
+
+        stmt.close();
+        conn.close();
+        return students;
     }
 }
