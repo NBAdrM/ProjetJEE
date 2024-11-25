4db5209b2b4898b912185b28f4bdd924469a7ef0
Adrien Maestri <adrien.maestri@gmail.com>
2024-11-25 13:26:45 +0100

    add Studenthascourse BDD

--- src/main/java/com/example/projetjee/models/Course.java
+++ src/main/java/com/example/projetjee/models/Course.java
@@ -7,13 +7,15 @@ import java.sql.SQLException;
 public class Course {
     private int id;
     private String name;
+    private int degree;
     private int year;
     private int teacherId;
     private String teacherName;
 
-    public Course(int id, String name, int year,int teacherId) throws SQLException, ClassNotFoundException {
+    public Course(int id, String name, int degree ,int year,int teacherId) throws SQLException, ClassNotFoundException {
         this.id = id;
         this.name = name;
+        this.degree = degree;
         this.year = year;
         this.teacherId = teacherId;
         this.teacherName = DbConnnect.getTeacher(teacherId).getLastName();
