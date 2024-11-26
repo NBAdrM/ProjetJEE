9e39a4084e6d1ae73d80aa54c1aa3c8d645f0356
Adrien Maestri <adrien.maestri@gmail.com>
2024-11-26 22:23:17 +0100

    add PdfGenerator

--- src/main/java/com/example/projetjee/models/Course.java
+++ src/main/java/com/example/projetjee/models/Course.java
@@ -20,5 +20,53 @@ public class Course {
         this.teacherId = teacherId;
         this.teacherName = DbConnnect.getTeacher(teacherId).getLastName();
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDegree() {
+        return degree;
+    }
+
+    public void setDegree(int degree) {
+        this.degree = degree;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
 }
 
