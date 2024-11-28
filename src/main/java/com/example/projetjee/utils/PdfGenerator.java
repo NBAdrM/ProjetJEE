a6f9ae2f27202af5f953d836425f2fc48fb86e10
Poulpino <louis.elisabeth@proton.me>
2024-11-28 16:57:37 +0100

    Revert "update"

--- src/main/java/com/example/projetjee/utils/PdfGenerator.java
+++ src/main/java/com/example/projetjee/utils/PdfGenerator.java
@@ -1,26 +1,18 @@
 package com.example.projetjee.utils;
 
-import com.example.projetjee.models.Grade;
-import com.example.projetjee.models.Student;
-import com.example.projetjee.models.StudentCourse;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.List;
-
-import static com.example.projetjee.utils.DbConnnect.*;
 
 public class PdfGenerator {
-    public static void generatBulltin(int id,int year) throws SQLException, ClassNotFoundException {
+    //TODO Rajouter la liste des notes
+    public static void generatBultin(int id,int year){
 
-        List<StudentCourse> studentCourses = DbConnnect.getStudentCoursesByYear(id,year);
-        Student student = DbConnnect.getStudent(id);
 
-        String dest = "BULLTIN_"+student.getFirstName().toUpperCase()+"_"+student.getLastName().toUpperCase()+"_"+year+"_"+(year+1)+".pdf";
+        String dest = "example.pdf";
 
         try (PDDocument document = new PDDocument()) {
             PDPage page = new PDPage();
@@ -32,7 +24,7 @@ public class PdfGenerator {
                 contentStream.beginText();
                 contentStream.setLeading(14.5f);
                 contentStream.newLineAtOffset(50, 750);
-                contentStream.showText("Année scolaire : "+year+"/"+(year+1));
+                contentStream.showText("Année scolaire : YYYY/YYYY");
                 contentStream.endText();
 
                 contentStream.setFont(PDType1Font.HELVETICA_BOLD, 18);
@@ -46,7 +38,7 @@ public class PdfGenerator {
                 contentStream.beginText();
                 contentStream.setLeading(14.5f);
                 contentStream.newLineAtOffset(350, 730);
-                contentStream.showText(student.getLastName().toUpperCase()+" "+student.getFirstName());
+                contentStream.showText("NOM Prenom");
                 contentStream.endText();
 
                 //Pour un ligne
@@ -74,63 +66,10 @@ public class PdfGenerator {
                 contentStream.moveTo(50, 670);
                 contentStream.lineTo(550, 670);
                 contentStream.stroke();
-
-                int xgrade = 60;
-                int xname = 350;
-                int yline = 650;
-                float moyenneGene = 0;
-                int nbCourse = 0;
-                for (StudentCourse sc: studentCourses){
-
-                    List<Grade> grades = sc.getGrades();
-                    if (grades.size()>0) {
-                        float moyenne = 0;
-                        int nbg = grades.size();
-                        for (Grade g:grades) {
-                            moyenne += g.getGrade();
-                        }
-                        moyenne = moyenne/nbg;
-                        moyenneGene += moyenne;
-                        nbCourse++;
-                        contentStream.setFont(PDType1Font.HELVETICA, 12);
-                        contentStream.beginText();
-                        contentStream.setLeading(14.5f);
-                        contentStream.newLineAtOffset(xgrade, yline);
-                        contentStream.showText(getCourseById(sc.getCourseId()).getName());
-                        contentStream.endText();
-
-                        contentStream.setFont(PDType1Font.HELVETICA, 12);
-                        contentStream.beginText();
-                        contentStream.setLeading(14.5f);
-                        contentStream.newLineAtOffset(xname, yline);
-                        contentStream.showText(String.valueOf(moyenne));
-                        contentStream.endText();
-
-                        yline -= 20;
-                    }
-                }
-                contentStream.setLineWidth(1);
-                contentStream.moveTo(50, yline+14);
-                contentStream.lineTo(550, yline+14);
-                contentStream.stroke();
-
-                moyenneGene = moyenneGene/nbCourse;
-                contentStream.setFont(PDType1Font.HELVETICA, 12);
-                contentStream.beginText();
-                contentStream.setLeading(14.5f);
-                contentStream.newLineAtOffset(xgrade, yline);
-                contentStream.showText("Moyenne générale");
-                contentStream.endText();
-
-                contentStream.setFont(PDType1Font.HELVETICA, 12);
-                contentStream.beginText();
-                contentStream.setLeading(14.5f);
-                contentStream.newLineAtOffset(xname, yline);
-                contentStream.showText(String.valueOf(moyenneGene));
-                contentStream.endText();
             }
 
             document.save(dest);
+            System.out.println("PDF créé avec succès !");
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/webapp/course/enrollment.jsp
+++ src/main/webapp/course/enrollment.jsp
@@ -11,16 +11,7 @@
 <body>
 <h1>Inscription à un Cours</h1>
 
-<%-- Affichage des messages d'erreur ou de succès --%>
-<div id="messageContainer">
-  <% if (session.getAttribute("errorMessage") != null) { %>
-  <p style="color: red;"><%= session.getAttribute("errorMessage") %></p>
-  <% session.removeAttribute("errorMessage"); %>
-  <% } else if (session.getAttribute("successMessage") != null) { %>
-  <p style="color: green;"><%= session.getAttribute("successMessage") %></p>
-  <% session.removeAttribute("successMessage"); %>
-  <% } %>
-</div>
+<div id="messageContainer" style="display: none;"></div>
 
 <form id="enrollForm" method="post" action="${pageContext.request.contextPath}/enrollment">
   <label for="courseId">Choisissez un cours :</label>
